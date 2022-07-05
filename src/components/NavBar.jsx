250d33d03eaf3a87cea58afd07dc8cab39efb4fb
francog <fghigo@hotmail.com.ar>
2022-07-05 17:14:44 -0300

    NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+const NavBar = (props) => {
+    console.log(props);
+    return (
+
+<nav className="navbar navbar-expand-xl navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Mi TiendA!</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDark" aria-controls="navbarDark" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse show" id="navbarDark">
+      <ul className="navbar-nav me-auto mb-2 mb-xl-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Productos</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">OfertAs!</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-light" type="submit">BuscAr!</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  );
+};
+
+export default NavBar;
