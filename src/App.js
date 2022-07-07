cf256eb5b73759e2f1b54e47bf0116942bfd1695
francog <fghigo@hotmail.com.ar>
2022-07-07 18:37:06 -0300

    carrito

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+
 import NavBar from './components/NavBar';
+
 
 function App() {
   const title = "Titulo de Tienda";
    return (
     <>
-      <NavBar />
-     
+      <NavBar/>
+    
+            
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,15 @@
+import CartWidget from "./CartWidget";
+
 const NavBar = (props) => {
     console.log(props);
     return (
 
-<nav className="navbar navbar-expand-xl navbar-dark bg-dark">
+<nav className="navbar navbar-expand-xl navbar-dark bg-primary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Mi TiendA!</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDark" aria-controls="navbarDark" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+    <a className="navbar-brand" href="#">Mi TiendA! </a>
+    
+     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDark" aria-controls="navbarDark" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse show" id="navbarDark">
       <ul className="navbar-nav me-auto mb-2 mb-xl-0">
@@ -17,14 +20,17 @@ const NavBar = (props) => {
           <a className="nav-link" href="#">Productos</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">OfertAs!</a>
+          <a className="nav-link" href="#">OfertAs! </a>
         </li>
       </ul>
+      <CartWidget/>
+      
       <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-light" type="submit">BuscAr!</button>
+        <input className="form-control me-2" type="search"  aria-label="Search"/>
+        <button className="btn btn-outline-light" type="submit">BuscAr! </button> 
       </form>
-    </div>
+      </div>
+      
   </div>
 </nav>
   );
