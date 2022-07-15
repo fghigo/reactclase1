537dc0e2f2ac0362e252a9f24a51b80f3585aff2
francog <fghigo@hotmail.com.ar>
2022-07-15 01:44:27 -0300

    contador terminado

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const ItemCount = ({ stock = 25, initial = 0}) => {
+const ItemCount = ({ stock = 25, initial = 0, onAdd}) => {
     const [count, setCount] = useState(initial);
     const contador = (op) => {
       if (op === "-" && count > 0) {
@@ -10,15 +10,32 @@ const ItemCount = ({ stock = 25, initial = 0}) => {
         setCount(count + 1);
       }
     };
+    const updateCountInput = (e) => {
+      const { value } = e.target;
+      if (value <= stock) {
+        setCount(isNaN(value) ? 0 : parseInt(value));
+      }
+    };
 
 return (
     <>
+     <input
+          onChange={(e) => updateCountInput(e)}
+          placeholder=""
+          value={count}
+          type="number"
+        />
     <button onClick={() => contador ("-")}>-</button>
     {count}
     <button onClick={() => contador ("+")}>+</button>
-    <button>ComprA!</button>
-    </>
+    <button  onClick={() => onAdd(count)} 
+          disabled={count === "" || count <= 0}
+          >ComprA!</button>
+          </>
 )
 };
+
+
+  
 
 export default ItemCount;
